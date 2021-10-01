f0f328eb9814882e10005de017375bb542ea1859
Stuart Wade <stewbawka@gmail.com>
2021-10-01 19:34:05 -0300

    Non functioning login form using tailwind applied component style from styleguide

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -17,5 +17,21 @@ export default {
 </script>
 
 <template>
-  <h1>Login Page {{ token }}</h1>
+<div class="flex justify-center bg-gray-300 pt-8">
+  <form class="grid grid-cols-1 gap-6">
+    <label class="block">
+      <span class="text-gray-700">Email</span>
+      <input type="email" class="control" />
+    </label>
+    <label class="block">
+      <span class="text-gray-700">Password</span>
+      <input type="password" class="control" />
+    </label>
+    <div>
+      <button class="my-4 px-4 py-2 border-2 border-black rounded-lg text-white bg-blue-900">
+        Login
+      </button>
+    </div>
+  </form>
+</div>
 </template>
