a756487e373dbce16592feeb67ca0fa0f5a64786
Stuart Wade <stewbawka@gmail.com>
2022-03-18 19:18:07 -0300

    Naviagation guard for requiresAuth

--- src/router.ts
+++ src/router.ts
@@ -2,16 +2,27 @@ import { createWebHistory, createRouter } from "vue-router";
 import Settings from "@/pages/Settings.vue";
 import Login from "@/pages/Login.vue";
 import Logout from "@/pages/Logout.vue";
+import { useStore } from '@/store';
 
 const routes = [
-  { path: "/login", exact: true, component: Login },
-  { path: "/me", exact: true, component: Settings },
-  { path: "/logout", exact: true, component: Logout },
+  { name: 'login', path: "/login", exact: true, component: Login },
+  { name: 'settings', path: "/me", exact: true, component: Settings, meta: { requiresAuth: true }},
+  { name: 'logout', path: "/logout", exact: true, component: Logout },
 ];
 
 const router = createRouter({
   history: createWebHistory(),
   routes
+});
+
+router.beforeEach((to, from) => {
+  const { meta } = to;
+  if (meta.requiresAuth) {
+    const { session } = useStore();
+    if (!session || !session.value.user) {
+      return { name: 'login', query: { next: to.path }}
+    }
+  }
 });
 
 export default router;
